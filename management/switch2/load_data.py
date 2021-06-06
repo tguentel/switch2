d7ad151b83e0df2e8cb1d99a4a16f81aa93f7b6b
Thore Guentel <thore.guentel@tangogolf.de>
2021-06-06 18:48:07 +0200

    removed wrong if statement

--- management/switch2/load_data.py
+++ management/switch2/load_data.py
@@ -12,8 +12,7 @@ from switch2 import redis_db0
 @app.route("/reload")
 def load_data():
     for datafile in glob.glob(app.config['JSON_PATH'] + '*.json'):
-        if datafile == "":
-        elif os.path.basename(datafile).split('.')[0] not in app.config['JSON_FILES']:
+        if os.path.basename(datafile).split('.')[0] not in app.config['JSON_FILES']:
             sys.stderr.write("Ignored json file: %s\n" % datafile)
         else:
             with open(datafile) as input:

3116abc458665fabb275cc3273c9be2b207bd808
Thore Guentel <thore.guentel@tangogolf.de>
2021-06-25 14:05:09 +0200

    Added more global values

--- management/switch2/config.py
+++ management/switch2/config.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
+PAGE_TITLE = "tangogolf Switch 2"
+
+RS_INITIAL_VALUE = 0.6
+TH_INITIAL_VALUE = 18.0
+
 REDIS_URL = "redis://switch2-redis:6379/0"
 JSON_PATH = "/json/"
 JSON_FILES = [ "rooms", "presets" ]

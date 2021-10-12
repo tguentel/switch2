5ab862cf7c9eeabbdc2dec29f3d244ea5553bd97
Thore Guentel <thore.guentel@tangogolf.de>
2021-10-12 16:45:56 +0200

    Added initial Value to Power-Outlet

---
+++ management/switch2/static/js/radio_checked.js
@@ -0,0 +1,5 @@
+if (current == "1") {
+    document.getElementById("radio_true_"+device).checked = true;
+} else if (current == "0") {
+    document.getElementById("radio_false_"+device).checked = true;
+}

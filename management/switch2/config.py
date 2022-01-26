55258e68e5c3503007609e6c4eb2415e0d248926
Thore Guentel <thore.guentel@tangogolf.de>
2022-01-26 10:54:39 +0100

    rework data capture strategy

--- management/switch2/config.py
+++ management/switch2/config.py
@@ -6,14 +6,46 @@ SEND_FILE_MAX_AGE_DEFAULT = 0
 
 REDIS_URL = "redis://switch2-redis:6379/0"
 
-HMIP_API_DEVICES = ["hmip-broll", "hmip-bwth", "hmip-wth-2", "hmip-psm", "hmip-fsm"]
-HMIP_API_TYPES = ["LEVEL", "SET_POINT_TEMPERATURE","STATE"]
-HMIP_API_BASE_URL = "http://192.168.54.75/addons/xmlapi/"
-HMIP_API_ROOMLIST = "roomlist.cgi"
-HMIP_API_STATELIST = "statelist.cgi"
-HMIP_API_FUNCTIONLIST = "functionlist.cgi"
-HMIP_API_STATE = "state.cgi"
+HMIP_API_BASE_URL = "http://192.168.54.75:2121"
+HMIP_API_DEVICE_URL = "/device"
 
-MHIP_MODEL_PO = ["hmip-psm", "hmip-fsm"]
-MHIP_MODEL_TH = ["hmip-wth-2", "hmip-bwth"]
-MHIP_MODEL_RS = ["hmip-broll"]
+HMIP_API_ALLOWED_DEVICES = [
+        "hmip-broll",
+        "hmip-bwth",
+        "hmip-wth-2",
+        "hmip-psm",
+        "hmip-fsm",
+        "hmip-swdo",
+        "hmip-fci1",
+        "hmip-srh"
+        ]
+
+HMIP_API_CONTROL_INDEX = {
+        "hmip-broll": 4,
+        "hmip-bwth": 1,
+        "hmip-wth-2": 1,
+        "hmip-psm": 3,
+        "hmip-fsm": 2
+        }
+
+HMIP_API_CURRENT_INDEX = {
+        "hmip-broll": 3,
+        "hmip-bwth": 1,
+        "hmip-wth-2": 1,
+        "hmip-psm": 2,
+        "hmip-fsm": 1,
+        "hmip-swdo": 1,
+        "hmip-fci1": 1,
+        "hmip-srh":: 1
+        }
+
+HMIP_API_ALLOWED_TYPES = [
+        "level",
+        "set_point_temperature",
+        "humidity",
+        "actual_temperature"
+        "state"
+        ]
+
+
+
--- management/switch2/load_data.py
+++ management/switch2/load_data.py
@@ -3,11 +3,18 @@
 import json
 import requests
 import xml.etree.ElementTree as ET
+import sys
 
 from flask import render_template
 
 from switch2 import app
 from switch2 import redis_db0
+
+
+
+def make_request(url):
+    r = requests.get(url)
+    return r.json()
 
 @app.route("/reload")
 def load_data():
@@ -15,116 +22,42 @@ def load_data():
     state_url = app.config['HMIP_API_BASE_URL'] + app.config['HMIP_API_STATELIST']
     function_url = app.config['HMIP_API_BASE_URL'] + app.config['HMIP_API_FUNCTIONLIST']
 
-    r = requests.get(room_url)
-    r_root = ET.fromstring(r.text)
+    devicelist = {}
 
-    s = requests.get(state_url)
-    s_root = ET.fromstring(s.text)
+    url = app.config['HMIP_API_BASE_URL'] + app.config['HMIP_API_DEVICE_URL']
+    devices = make_request(url)
+    for device in devices['~links']:
+        devicedata = make_request("%s/%s" % (url, device['href']))
+        try:
+            devicedata['type']
+        except:
+            pass
+        else:
+            if devicedata['type'].lower() in app.config['HMIP_API_ALLOWED_DEVICES']:
+                d_address = devicedata['address']
+                d_type = devicedata['type'].lower()
+                d_title = devicedata['title']
+                devicelist.update({d_address: {'title': d_title, 'type': d_type, 'room': {}, 'function': {}, 'channel': {}}})
+                for d_channel in devicedata['~links']:
+                    if d_channel['rel'] == "channel" and d_channel['href'] != "$MASTER":
+                        #sys.stdout.write(str(channel['href']))
+                        channeldata = make_request("%s/%s/%s" % (url, d_address, d_channel['href']))
+                        for c_links in channeldata['~links']:
+                            if c_links[]
+                            if c_links['rel'] == "room":
+                                devicelist[d_address]['room'].update({c_links['href'].replace('/room/',''): c_links['title']})
+                            elif c_links['rel'] == "function":
+                                devicelist[d_address]['function'].update({c_links['href'].replace('/function/',''): c_links['title']})
+                            elif c_links['rel'] == "parameter" and c_links['href'] in app.config['HMIP_API_ALLOWED_TYPES']:
+                                parameter = make_request("%s/%s/%s/%s/~pv" % (url, d_address, d_channel['href'], c_links['href'].upper()))
+                                devicelist[d_address]['channel'].update
+        sys.stdout.write(str(devicelist))
 
-    f = requests.get(function_url)
-    f_root = ET.fromstring(f.text)
+        #for d in devicedata:
+            #if devicedata[d]['type'].lower() in app.config['HMIP_API_ALLOWED_DEVICES']:
+                #sys.stdout.write(str(devicedata[d]['title']) + "\n")
 
-    roomlist = {'rooms': {}}
-    for r in r_root:
-        jr = json.loads(str(r.attrib).replace("'",'"'))
-        room = jr['ise_id']
-        name = jr['name']
-        roomlist['rooms'].update({room: {'name': name, 'channels': []}})
-        for c in r:
-            jc = json.loads(str(c.attrib).replace("'",'"'))
-            roomlist['rooms'][room]['channels'].append(jc['ise_id'])
-
-    functionlist = {'functions': {}}
-    for f in f_root:
-        jf = json.loads(str(f.attrib).replace("'",'"'))
-        function = jf['ise_id']
-        name = jf['name']
-        functionlist['functions'].update({function: {'name': name, 'channels': []}})
-        for c in f:
-            jc = json.loads(str(c.attrib).replace("'",'"'))
-            functionlist['functions'][function]['channels'].append(jc['ise_id'])
-
-    devicelist = {'devices': {}}
-    for s in s_root:
-        jde = json.loads(str(s.attrib).replace("'",'"'))
-        device = jde['ise_id']
-        name = jde['name']
-        devicelist['devices'].update({device: {'name': name, 'index': {}, 'room': {}, 'function': []}})
-        for c in s:
-            jdc = json.loads(str(c.attrib).replace("'",'"'))
-            index = jdc['index']
-            channel = jdc['ise_id']
-            model = jdc['name'].split(' ')[0]
-            if model.lower() in app.config['HMIP_API_DEVICES']:
-                devicelist['devices'][device].update({'model': model.lower()})
-                for d in c:
-                    jda = json.loads(str(d.attrib).replace("'",'"'))
-                    datapoint = jda['ise_id']
-                    if jda['type'] in app.config['HMIP_API_TYPES']:
-                        devicelist['devices'][device]['index'].update({index: {}})
-                        devicelist['devices'][device]['index'][index].update({'channel': channel})
-                        devicelist['devices'][device]['index'][index].update({'datapoint': datapoint})
-                        for room in roomlist['rooms']:
-                            if channel in roomlist['rooms'][room]['channels']:
-                                devicelist['devices'][device]['room'].update({
-                                    'id': room,
-                                    'name': roomlist['rooms'][room]['name']
-                                    })
-                        for function in functionlist['functions']:
-                            if channel in functionlist['functions'][function]['channels']:
-                                devicelist['devices'][device]['function'].append({
-                                    'id': function,
-                                    'name': functionlist['functions'][function]['name']
-                                    })
-
-    redis_db0.set("devicelist", json.dumps(devicelist))
+    #redis_db0.set("devicelist", json.dumps(devicelist))
 
     return "Daten neu geladen\n"
-
-
-def gather_current_values(url):
-    s = requests.get(url)
-    s_root = ET.fromstring(s.text)
-
-    for s in s_root:
-        jda = json.loads(str(s.attrib).replace("'",'"'))
-        value = jda['value']
-
-    try:
-        float(value)
-    except:
-        return str(value)
-    else:
-        return str(round(float(value), 2))
-
-@app.route("/update")
-def update_states():
-    state_url = app.config['HMIP_API_BASE_URL'] + app.config['HMIP_API_STATE'] + "?datapoint_id=%s"
-    devices = redis_db0.get('devicelist')
-    currentvalues = {'values': {}}
-
-    if devices == None:
-        return "Keine Ger&auml;te gefunden. <a href='/reload'>Reload ausf&uuml;hren.</a>"
-
-    else:
-        jde = json.loads(devices)
-        for d in jde['devices']:
-            try:
-                jde['devices'][d]['model']
-            except:
-                pass
-            else:
-                if jde['devices'][d]['model'] == "hmip-broll":
-                    index = "3"
-                elif jde['devices'][d]['model'] == "hmip-bwth" or jde['devices'][d]['model'] == "hmip-wth-2":
-                    index = "1"
-                elif jde['devices'][d]['model'] == "hmip-psm" or jde['devices'][d]['model'] == "hmip-fsm":
-                    index = "2"
-                datapoint = jde['devices'][d]['index'][index]['datapoint']
-                value = gather_current_values(state_url % datapoint)
-                currentvalues['values'].update({d: value })
-
-    redis_db0.set("currentvalues", json.dumps(currentvalues))
-
-    return "Update ausgeführt\n"
 
